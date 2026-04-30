dd44ecec51c90e5c36af1a9685cd50e6f7b8255d
kwonjunin <kwonyoungjun0521@gmail.com>
2026-04-30 15:46:21 +0900

    260430 first commit

---
+++ routes/connect.js
@@ -0,0 +1,19 @@
+//260402 DB 연결
+const oracledb = require('oracledb');
+
+async function getConnection() {
+    let connection;
+    try{
+        connection = await oracledb.getConnection({
+            user: 'user205',
+            password:'pass',
+            connectString:'172.18.7.134:1521/xe'
+        });
+        console.log('DB 연결 성공');
+        return connection;
+    }catch(err){
+        console.log('DB연결 오류:', err.message);
+    }
+}
+
+module.exports = {getConnection};
