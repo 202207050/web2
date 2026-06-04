bdb0a2e3205ea82e4dc97fcac55569b408a1c553
202207050 <kwonyoungjun0521@gmail.com>
2026-06-04 12:58:29 +0900

    260528_3

--- routes/posts.js
+++ routes/posts.js
@@ -13,18 +13,21 @@ router.get('/', function(req, res, next) {
 router.get('/list.json', async function(req, res){
     let page = parseInt(req.query.page) || 1;
     let size = parseInt(req.query.size) || 5;
+    let word = req.query.word || '';
     let off_rows = (page-1) * size;
     let con;
     try{
         con = await getConnection();
 
-        let sql="SELECT * FROM VIEW_POSTS";
-            sql+="  ORDER BY ID DESC";
+        let sql="SELECT * FROM VIEW_POSTS ";
+            sql+=` WHERE TITLE LIKE '%${word}%' OR CONTENT LIKE '%${word}%' OR SNAME LIKE '%${word}%'`;
+            sql+="  ORDER BY ID DESC ";
             sql+=` OFFSET ${off_rows} ROWS FETCH NEXT ${size} ROWS ONLY`;
         let result = await con.execute(sql, {}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
         let list = result.rows;
-        
-        sql = "select count(*) from view_posts";
+
+        sql = " select count(*) from view_posts ";
+        sql += ` WHERE TITLE LIKE '%${word}%' OR CONTENT LIKE '%${word}%' OR SNAME LIKE '%${word}%'`;
         result = await con.execute(sql);
         let count=result.rows[0][0];
 
@@ -36,35 +39,9 @@ router.get('/list.json', async function(req, res){
     }
 });
 
-module.exports = router;
-
-
-
-/*
-//게시글 목록 데이터
-router.get('/list.json', async function(req, res){
-    let con;
-    const page=parseInt(req.query.page);
-    const size=parseInt(req.query.size);
-    const off_rows = (page-1) * size;
-    try{
-        con = await getConnection();
-        let sql = "select * from view_posts order by id desc "
-            sql+= `offset ${off_rows} rows fetch next ${size} rows only`;
-        let result = await con.execute(sql, {}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
-        const list = result.rows;
-
-        sql = "select count(*) from posts";
-        result = await con.execute(sql);
-        const count = result.rows[0][0];
-
-        res.send({list, count});
-    }catch(err){
-        console.log('게시글 목록 데이터', err.message);
-    }finally{
-        if(con) await con.close();
-    }
+//글쓰기 페이지 이동
+router.get('/insert', function(req, res){
+    res.render('index', {title: '글쓰기', pageName:'posts/insert.ejs'});
 });
 
 module.exports = router;
-*/
