4c7c397b36d8f458d3ec6ca1de456543251d6101
202207050 <kwonyoungjun0521@gmail.com>
2026-06-04 15:42:48 +0900

    260604 maybe last

--- routes/posts.js
+++ routes/posts.js
@@ -44,4 +44,96 @@ router.get('/insert', function(req, res){
     res.render('index', {title: '글쓰기', pageName:'posts/insert.ejs'});
 });
 
+//게시글 등록
+router.post('/insert', async function(req, res) {
+    const title=req.body.title;
+    const content=req.body.content;
+    const writer=req.body.writer;
+    //console.log(title, content, writer);
+    let con;
+    try{
+        con = await getConnection();
+        let sql="insert into posts(title, content, writer) values(:title, :content, :writer)";
+        await con.execute(sql, {title, content, writer}, {autoCommit:true});
+
+    }catch(err){
+        console.log('게시글 등록', err.message);
+        res.sendStatus(500);
+    }finally{
+        if(con) await con.close();
+    }
+    res.sendStatus(200);
+});
+
+//게시글 정보페이지
+router.get('/:id', async function(req, res){
+    const id = req.params.id;
+    let con;
+    try{
+        con = await getConnection();
+        let sql="select * from view_posts where id=:id";
+        let result = await con.execute(sql, {id}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+        let post = result.rows[0];
+        res.render('index', {title:'게시글 정보', pageName:'posts/read.ejs', post});
+    }catch{
+        console.log('게시글 정보', err.message);
+        res.sendStatus(500);
+    }finally{
+        if(con) await con.close();
+    }
+});
+
+//게시글 삭제
+router.post('/delete', async function(req, res){
+    const id=req.body.id;
+    let con;
+    try{
+        con = await getConnection();
+        let sql ="delete from posts where id=:id";
+        await con.execute(sql, {id}, {autoCommit:true});
+        res.sendStatus(200);
+    }catch(err){
+        console.log('게시글 삭제', err.message);
+        res.sendStatus(500);
+    }finally{
+        if(con) await con.close();
+    }
+});
+
+//게시글 수정 페이지
+router.get("/update/:id", async function(req, res){
+    const id=req.params.id;
+    let con;
+    try{
+        con = await getConnection();
+        let sql="select * from view_posts where id=:id";
+        let result = await con.execute(sql, {id}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+        let post = result.rows[0];
+        console.log(post);
+        res.render('index', {title:'게시글 수정', pageName:'posts/update.ejs', post});
+    }catch(err){
+        console.log('게시글 수정', err.message)
+    }finally{
+        if(con) await con.close();
+    }
+});
+
+//게시글 수정
+router.post('/update', async function(req, res){
+    const id=req.body.id;
+    const title=req.body.title;
+    const content=req.body.content;
+    let con;
+    try{
+        con= await getConnection();
+        let sql="update posts set title=:title, content=:content where id=:id";
+        await con.execute(sql, {title, content, id}, {autoCommit:true});
+        res.sendStatus(200);
+    }catch(err){
+        console.log('게시글 수정', err.message);
+        res.sendStatus(500);
+    }finally{
+        if(con) await con.close();
+    }
+});
 module.exports = router;
